c04dfad6288d91593a5168f225c030a2f316e29e
David Chan <davidkfc@gmail.com>
2023-09-12 10:46:46 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// import { flag, touch } from './validator.js';
+// console.log(flag);
+// touch();
+// console.log(flag);
+
+
+import {halfOf, multiply} from './lib.js';
+console.log(halfOf(84));
+console.log(multiply(22,2));
+
+
+import doSomething from './doSomething.js';
+doSomething();
